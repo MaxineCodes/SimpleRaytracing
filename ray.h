f4bc90fe0f5d46d6a15c6920ab1011f53219d15c
Maxine <89635193+MaxineCodes@users.noreply.github.com>
2022-09-14 10:00:10 +0200

    Initial Commit
    So far it displays a background, and a sphere with normals.

---
+++ ray.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#define RAY_H
+
+#include "vec3.h"
+
+class ray 
+{
+
+    public:
+
+        ray() 
+        {
+
+        }
+
+        ray(const point3& origin, const vec3& direction)
+            : m_origin(origin), m_direction(direction) 
+        {
+
+        }
+
+        point3 origin() const 
+        { 
+            return m_origin; 
+        }
+
+        vec3 direction() const 
+        { 
+            return m_direction; 
+        }
+
+        point3 at(double t) const 
+        {
+            return m_origin + t * m_direction;
+        }
+
+    public:
+
+        point3 m_origin;
+        vec3 m_direction;
+
+};
